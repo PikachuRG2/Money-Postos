3bc9c8c39e7c1efbb7608f542db20a1fa2402f36
Rodrigo <rafael2019rg@gmail.com>
2026-02-24 22:06:03 -0300

    h
    h

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Configuração do Firebase (Substitua pelos seus dados do console do Firebase)
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    projectId: "SEU_PROJETO",
+    storageBucket: "SEU_PROJETO.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
